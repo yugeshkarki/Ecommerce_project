f7ab6f5fea28ddb407d8fc6ee7d9cc069e6f672f
yugesh karki <yugeshkarki1@gmail.com>
2025-11-11 19:21:40 +0545

    additional code updated

--- src/pages/ShopCategory.jsx
+++ src/pages/ShopCategory.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
-
-const shopcategory = () => {
+import React,{useContext} from 'react';
+import "./CSS/ShopCategory.css"
+import { ShopContext } from '../context/ShopContext'
+import dropdown_icon from '../components/Assets/dropdown_icon.png'
+import Item from '../components/Item/Item';
+const ShopCategory = (props) => {
+  const{all_product}= useContext(ShopContext)
   return (
-    <div>
-      
+    <div className='Shop-category'>
+      <img src={props.banner} alt=""/>
+      <div className="Shopcategory-indexSort">
+        <p>
+          <span>Showing 1-12</span> out of 36 products
+        </p>
+        <div className="Shopcategory-sort">
+          sort by <img src={dropdown_icon} alt=''/>
+        </div>
+      </div>
+      <div className="Shopcategory-products">
+        {all_product.filter(
+    (item) =>
+      item.category.toLowerCase() === props.category.toLowerCase()
+  ).map((item,i)=>{
+       if(props.category===item.category){
+        return <Item  key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
+       }
+       else{
+        return null;
+       }
+        })}
+        
+      </div>
     </div>
   )
 }
 
-export default shopcategory
+export default ShopCategory
