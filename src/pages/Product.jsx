38826cbf583c3aa22792d7325124f4d71b4067d3
yugesh karki <yugeshkarki1@gmail.com>
2025-11-13 20:56:49 +0545

    updated productdisplay page

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ShopContext } from '../context/ShopContext'
+import { useParams } from 'react-router-dom';
+import Breadcrum from '../components/Breadcrums/Breadcrum';
+import ProductDisplay from '../components/ProductDisplay/ProductDisplay';
 
-const product = () => {
+const Product = () => {
+  const{all_product} =useContext(ShopContext);
+  const{productId} =useParams();
+  const product =all_product.find((e)=>e.id=== Number(productId));
+
   return (
     <div>
-      
+      <Breadcrum product={product}/>
+      <ProductDisplay product={product}/>
     </div>
   )
 }
 
-export default product
+export default Product
